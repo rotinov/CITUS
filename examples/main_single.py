b0d33e951fbd4c2c386bf44ccae9c4721c314807
rotinov <phantom+Tracy@github.com>
2019-10-01 20:01:36 +0300

    Greate perfomance improve

--- examples/main_single.py
+++ examples/main_single.py
@@ -8,7 +8,7 @@ env_name3 = "Swimmer-v2"
 
 
 if __name__ == '__main__':
-    envs = agnes.make_vec_env(env_name1, envs_num=4)
+    envs = agnes.make_vec_env(env_name1, envs_num=16)
 
     runner = agnes.Single(envs, agnes.PPO, agnes.CNN)
     runner.log(agnes.TensorboardLogger(".logs/"+str(time.time())), agnes.log)
